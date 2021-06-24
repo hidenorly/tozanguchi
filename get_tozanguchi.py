21f219caed329efd9b9e35b389dfc70ea8192028
hidenorly <twitte.harold@gmail.com>
2021-06-24 19:47:33 +0900

    Add parsing functionality of the park info

--- get_tozanguchi.py
+++ get_tozanguchi.py
@@ -37,6 +37,49 @@ def ljust_jp(value, length, pad = " "):
       count_length += 2
   return value + pad * (length-count_length)
 
+def maintainParkInfo(result):
+  if "主要登山ルート" in result:
+    routes = result["主要登山ルート"].split("）")
+    newRoutes = []
+    for aRoute in routes:
+      aRoute = aRoute.strip()
+      if aRoute:
+        newRoutes.append( aRoute+")" )
+    result["主要登山ルート"] = newRoutes
+  return result
+
+def getParkInfo(url):
+  result = {}
+  res = requests.get(url)
+  soup = BeautifulSoup(res.text, 'html.parser')
+  if None != soup:
+    dts = soup.find_all("dt")
+    dds = soup.find_all("dd")
+    # i should be starting from 0 and shoud use dts[i], dds[i] and should not require to split
+    i = 1
+    while( i<len(dts) and i<len(dds) ):
+      key = dts[i*2-1].text.strip().split("\n")[0].strip()
+      value = dds[i].text.strip().split("\n")[0].strip()
+      result[key] = value
+      i=i+1
+
+  return maintainParkInfo(result)
+
+def showListAndDic(result, indent, startIndent):
+  for key, value in parkInfo.items():
+    if isinstance(value, list):
+      print(" "*startIndent + ljust_jp(key, indent-startIndent) + " : ", end="")
+      firstLine = True
+      for aValue in value:
+        if firstLine:
+          print( str(aValue) )
+          firstLine = False
+        else:
+          print(" "*(indent+3) + str(aValue) )
+    else:
+      print("    " + ljust_jp(key,indent-startIndent) + " : " + str(value))
+  print("")
+
 if __name__=="__main__":
   parser = argparse.ArgumentParser(description='Parse command line options.')
   parser.add_argument('args', nargs='*', help='mountain name such as 富士山')
@@ -59,3 +102,5 @@ if __name__=="__main__":
     tozanguchi = tozanguchiDic[aMountain]
     for aTozanguchi, theUrl in tozanguchi.items():
       print( "  " + ljust_jp(aTozanguchi, 18) + " : " + theUrl )
+      parkInfo = getParkInfo(theUrl)
+      showListAndDic(parkInfo, 20, 4)
---
+++ updateDic.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python3 tozanguchi_list_mountains.py "https://tozanguchinavi.com/mt" "https://tozanguchinavi.com/mt/page/" 2 95 > tozanguchiDic.py
